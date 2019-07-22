5d7333834e4f4f86054d4785f5cf02a733d3d706
Alexander Riccio <test35965+learn@gmail.com>
2019-07-22 10:52:53 -0400

    solve lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+
+function findMatching(drivers, name) {
+    return drivers.filter((driver) => {return driver.toLowerCase() === name.toLowerCase()});
+}
+
+function fuzzyMatch(drivers, name) {
+    return drivers.filter(driver => {return driver.startsWith(name)});
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(driver => {return driver.name.toLowerCase() === name.toLowerCase();});
+}
